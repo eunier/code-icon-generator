8c98523bd1afb36e1748040b095220a3df67226c
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-18 23:17:45 -0400

    [2023-05-19T03:17:45.337Z] Sync from gitlab.com/yunieralvarez/workspace

--- code-icon-generator-backend/src/app/app.controller.ts
+++ code-icon-generator-backend/src/app/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from 'app/app.service';
+import { AxiosResponse } from 'axios';
 
 @Controller ()
 export class AppController {
   constructor (private readonly _service: AppService) {}
 
   @Get ()
-  getData (): { message: string } {
+  public getData (): { message: string } {
     return this._service.getData ();
+  }
+
+  @Get ('icons')
+  public getIcons (): Promise<AxiosResponse<unknown, unknown>> {
+    return this._service.getIcons ();
   }
 }
