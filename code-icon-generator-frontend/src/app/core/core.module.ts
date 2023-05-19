746d616efc4eac1b4a6e493cee018892d7aaba15
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-18 23:37:47 -0400

    [2023-05-19T03:37:47.491Z] Sync from gitlab.com/yunieralvarez/workspace

---
+++ code-icon-generator-frontend/src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IconService } from 'app/core/services/icon/icon.service';
+
+@NgModule ({
+  declarations: [],
+  imports: [CommonModule],
+  providers: [IconService],
+})
+export class CoreModule {}
---
+++ code-icon-generator-frontend/src/app/modules/code-icon-generator/pages/code-icon-generator-page/code-icon-generator-page.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CodeIconGeneratorComponentModule } from 'app/modules/code-icon-generator/components/code-icon-generator/code-icon-generator.module';
+import { CodeIconGeneratorPageComponent } from 'app/modules/code-icon-generator/pages/code-icon-generator-page/code-icon-generator-page.component';
+
+@NgModule ({
+  declarations: [CodeIconGeneratorPageComponent],
+  imports: [CodeIconGeneratorComponentModule, CommonModule],
+  exports: [CodeIconGeneratorPageComponent],
+})
+export class CodeIconGeneratorPageComponentModule {}
