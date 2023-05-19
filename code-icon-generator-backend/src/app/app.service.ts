51ba659246fec3981433930a05592ac6553d1fc2
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-18 23:02:45 -0400

    [2023-05-19T03:02:45.086Z] Sync from gitlab.com/yunieralvarez/workspace

--- code-icon-generator-backend/src/app/app.service.ts
+++ code-icon-generator-backend/src/app/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable ()
 export class AppService {
+  constructor (private readonly _http: HttpServi){}
   getData (): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  getIcons (): unknown {
+    return ''
   }
 }
--- code-icon-generator-frontend/src/app/modules/code-icon-generator/code-icon-generator.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { codeIconGeneratorRoutes } from 'app/modules/code-icon-generator/code-icon-generator.routes';
-
-@NgModule ({
-  imports: [CommonModule, RouterModule.forChild (codeIconGeneratorRoutes)],
-})
-export class CodeIconGeneratorModule {}
