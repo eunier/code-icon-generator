f9031750c98d0e6328e7a7fe19e4da52fe0f872d
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-18 22:24:55 -0400

    [2023-05-19T02:24:55.858Z] Sync from gitlab.com/yunieralvarez/workspace

---
+++ code-icon-generator-frontend/src/app/modules/code-icon-generator/code-icon-generator.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CodeIconGeneratorPageComponent } from 'app/modules/code-icon-generator/pages/code-icon-generator-page/code-icon-generator-page.component';
+
+export const codeIconGeneratorRoutes: Routes = [
+  { path: '', component: CodeIconGeneratorPageComponent },
+];
