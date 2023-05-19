d9b7f442b3fa4c1a5a88a1f19b64f4c7818ec44b
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-18 22:37:31 -0400

    [2023-05-19T02:37:31.246Z] Sync from gitlab.com/yunieralvarez/workspace

--- code-icon-generator-frontend/src/app/modules/code-icon-generator/pages/code-icon-generator-page/code-icon-generator-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component ({
-  selector: 'app-code-icon-generator-page',
-  templateUrl: './code-icon-generator-page.component.html',
-  styleUrls: ['./code-icon-generator-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CodeIconGeneratorPageComponent {}
