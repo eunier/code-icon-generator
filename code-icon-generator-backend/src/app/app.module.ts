8e4677b96d1324f071a06bebee7f3e50113638ef
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-19 21:57:30 -0400

    [2023-05-20T01:57:30.914Z] Sync from gitlab.com/yunieralvarez/workspace

--- code-icon-generator-backend/src/app/app.module.ts
+++ code-icon-generator-backend/src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from 'app/app.controller';
-import { AppService } from 'app/app.service';
+import { AppService } from 'app/services/app.service';
 
 @Module ({
   imports: [HttpModule],
