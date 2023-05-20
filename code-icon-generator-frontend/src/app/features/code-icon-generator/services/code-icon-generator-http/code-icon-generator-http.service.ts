bea74f837b3ebc1cfcc8983961ca195fc3c79c73
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-19 22:19:52 -0400

    [2023-05-20T02:19:52.026Z] Sync from gitlab.com/yunieralvarez/workspace

---
+++ code-icon-generator-frontend/src/app/features/code-icon-generator/services/code-icon-generator-http/code-icon-generator-http.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable ()
+export class CodeIconGeneratorHttpService {
+  constructor (private readonly _http: HttpClient) {}
+  public getIcons (): Observable<unknown> {
+    return this._http.get ('api/icons');
+    // return this._http.get ('https://jsonplaceholder.typicode.com/posts/1')
+  }
+}
