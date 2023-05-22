09050563caa57dab0d530b37b27495328c248a91
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-22 15:58:28 -0400

    [2023-05-22T19:58:28.250Z] Sync from gitlab.com/yunieralvarez/workspace

---
+++ code-icon-generator-frontend/src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { IconService } from './services/icon/icon.service';
+// import { RxjsService } from './services/rxjs/rxjs.service';
+import {NgxRxjsModule} from '@workspace/ngx-rxjs'
+
+@NgModule ({
+  providers: [IconService, NgxRxjsModule /* RxjsService*/],
+})
+export class CoreModule {
+  constructor (
+    @Optional () @SkipSelf () private readonly _codeModule: CoreModule,
+  ) {
+    if (this._codeModule) {
+      throw new Error ('You should import core module only in the root module');
+    }
+  }
+}
