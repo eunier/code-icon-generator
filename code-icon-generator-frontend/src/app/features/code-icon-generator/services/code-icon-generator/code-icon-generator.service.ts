3c6ce1c982453b6f4c9bf07d4fc23914e745a085
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-23 10:44:52 -0400

    [2023-05-23T14:44:52.896Z] Sync from gitlab.com/yunieralvarez/workspace

---
+++ code-icon-generator-frontend/src/app/features/code-icon-generator/services/code-icon-generator/code-icon-generator.service.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable } from '@angular/core';
+import { RXJS_TOKEN, Rxjs } from '@workspace/ngx-rxjs';
+import { CodeIconGeneratorHttpService } from '../code-icon-generator-http/code-icon-generator-http.service';
+import { CodeIconGeneratorStoreService } from '../code-icon-generator-store/code-icon-generator-store.service';
+
+@Injectable ()
+export class CodeIconGeneratorService {
+  constructor (
+    @Inject (RXJS_TOKEN) private readonly _rxjs: Rxjs,
+    private readonly _http: CodeIconGeneratorHttpService,
+    private readonly _store: CodeIconGeneratorStoreService,
+  ) {}
+
+  public loadIcons (): void {
+    // TODO: return the array of string on the back end
+    this._http
+      .getIcons ()
+      .pipe (
+        this._rxjs.tap ((val) => console.log (val)),
+        this._rxjs.map ((res) => res.map ((el) => window.btoa (el))),
+        this._rxjs.tap ((res) => (this._store.icons = res)),
+        this._rxjs.tap ((val) => console.log (val)),
+      )
+      .subscribe ()
+      .unsubscribe ();
+  }
+}
