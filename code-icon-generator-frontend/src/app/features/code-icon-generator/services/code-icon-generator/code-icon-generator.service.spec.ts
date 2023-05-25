56d48e62be3db4c20419f936fd1fffa9038e9512
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-25 14:00:51 -0400

    [2023-05-25T18:00:51.945Z] Sync from gitlab.com/yunieralvarez/workspace

---
+++ code-icon-generator-frontend/src/app/features/code-icon-generator/services/code-icon-generator/code-icon-generator.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CodeIconGeneratorService } from './code-icon-generator.service';
+
+describe ('CodeIconGeneratorService', () => {
+  let service: CodeIconGeneratorService;
+
+  beforeEach (() => {
+    TestBed.configureTestingModule ({});
+    service = TestBed.inject (CodeIconGeneratorService);
+  });
+
+  it ('should be created', () => {
+    expect (service).toBeTruthy ();
+  });
+});
