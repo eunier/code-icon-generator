89c651a15339653f24894e88271d1a456872ef31
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-26 19:21:42 -0400

    [2023-05-26T23:21:42.862Z] Sync from gitlab.com/yunieralvarez/workspace

--- code-icon-generator-frontend-e2e/src/e2e/code-icon-generator-page.component.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('code-icon-generator-frontend', () => {
-  beforeEach(() =>
-    cy.visit('/iframe.html?id=codeicongeneratorpagecomponent--primary')
-  );
-  it('should render the component', () => {
-    cy.get('app-code-icon-generator-page').should('exist');
-  });
-});
