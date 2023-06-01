2d4a26dacb3421b1a50ad67a9d58206f583a6323
Yunier Alvarez <yunieralvarez@gmail.com>
2023-05-31 21:24:09 -0400

    [2023-06-01T01:24:09.377Z] Sync from gitlab.com/yunieralvarez/workspace

--- code-icon-generator-frontend/src/app/features/code-icon-generator/mocks/icons.mock.ts
+++
@@ -1,6 +0,0 @@
-import * as mock from '@workspace/code-icon-generator/mocks';
-import { Icon } from '../interfaces/icon.interface';
-
-export const icons = mock.files.map (
-  (file): Partial<Icon> => ({ file, svg: window.atob (file.content) }),
-);
